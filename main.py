a14df9c8f4b6b488cccd4ea5a80ef83e17ae7316
junghyun3369 <leesang3369@gmail.com>
2025-07-21 11:13:12 +0900

    함수2

--- main.py
+++ main.py
@@ -13,14 +13,26 @@
 
 b = 0
 
-def a(b):
-    print(b)
-    if b > 9:
-        pass
-    else :
-        a(b + 1)
+def a(b,c):
+    print(b,c)
+    if c >= 9:
+        c = 0
+        b += 1
+        if b > 9:
+            pass #종료
+        else:
+            a(b+1,c) #첫번째 변수 증가(밖)
+    else:
+        a(b,c + 1)# 두번째 변수 증가(안)   
+        # else :
+        #     a(b + 1)
+# a(0,0) ###########함수 반복문 기초(if)
 
-a(0) ###########함수 반복문 기초(if)
+# for b in range(10):
+#     print(b) ########함수 반복문 심화(for), 그리고 짧아서 자주 사용됨.
 
-for b in range(10):
-    print(b) ########함수 반복문 심화(for), 그리고 짧아서 자주 사용됨.
+for b in range(10): #10번 >> 0,1,2,3,4,5,6,7,8,9,
+    for c in range(10): # 10 *10번
+        print(b,c)
+
+    
