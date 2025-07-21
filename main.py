ebb1a4f8b8a4c89a75939ae85a6b5831e5e56424
junghyun3369 <leesang3369@gmail.com>
2025-07-21 11:45:30 +0900

    함수야

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-# print("함수 출력")
+# # print("함수 출력")
 
-# def a():
-#     print("a() 실행")
+# # def a():
+# #     print("a() 실행")
 
-# a()
+# # a()
 
-# def b():
-#     pass
+# # def b():
+# #     pass
 
-# b()
-# print("b() 호출 후 출력") //////////함수 기초
+# # b()
+# # print("b() 호출 후 출력") //////////함수 기초
 
-b = 0
+# b = 0
 
-def a(b,c):
-    print(b,c)
-    if c >= 9:
-        c = 0
-        b += 1
-        if b > 9:
-            pass #종료
-        else:
-            a(b+1,c) #첫번째 변수 증가(밖)
-    else:
-        a(b,c + 1)# 두번째 변수 증가(안)   
-        # else :
-        #     a(b + 1)
-# a(0,0) ###########함수 반복문(if), 길어서 어려워
+# def a(b,c):
+#     print(b,c)
+#     if c >= 9:
+#         c = 0
+#         b += 1
+#         if b > 9:
+#             pass #종료
+#         else:
+#             a(b+1,c) #첫번째 변수 증가(밖)
+#     else:
+#         a(b,c + 1)# 두번째 변수 증가(안)   
+#         # else :
+#         #     a(b + 1)
+# # a(0,0) ###########함수 반복문(if), 길어서 어려워
 
-# for b in range(10):
-#     print(b) ########함수 반복문(for),짧아서 자주 사용됨.
+# # for b in range(10):
+# #     print(b) ########함수 반복문(for),짧아서 자주 사용됨.
 
-for b in range(10): #10번 >> 0,1,2,3,4,5,6,7,8,9,
-    for c in range(10): # 10 *10번
-        print(b,c)
+# for b in range(10): #10번 >> 0,1,2,3,4,5,6,7,8,9,
+#     for c in range(10): # 10 *10번
+#         print(b,c)
 
     
+
+# def 함수이름(num : int):
+#     print(type(num))
+#     return num
+# print(type(함수이름),함수이름('1a'))
+
+def 함수이름(num):
+   print(type(num))
+   return num
+
+변수 = 함수이름(1)
+print(변수 * 9)
