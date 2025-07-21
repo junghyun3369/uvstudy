d31bbe7f1ba72fe34f96c34bc39d8c7ec4526ead
junghyun3369 <leesang3369@gmail.com>
2025-07-21 11:00:19 +0900

    함수...

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from uvstudy!")
+# print("함수 출력")
 
+# def a():
+#     print("a() 실행")
 
-if __name__ == "__main__":
-    main()
+# a()
+
+# def b():
+#     pass
+
+# b()
+# print("b() 호출 후 출력") //////////함수 기초
+
+b = 0
+
+def a(b):
+    print(b)
+    if b > 9:
+        pass
+    else :
+        a(b + 1)
+
+a(0) ###########함수 반복문 기초(if)
+
+for b in range(10):
+    print(b) ########함수 반복문 심화(for), 그리고 짧아서 자주 사용됨.
---
+++ study01/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello from study01!")
+
+
+    main()
